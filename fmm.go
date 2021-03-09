6299b25866f73e588653929301e7d3ec7fae64d7
RuscalWorld <me@ruscalworld.ru>
2021-03-09 19:20:49 +0300

    Basic compatibility checks

--- fmm.go
+++ fmm.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/RuscalWorld/FabricModManager/config"
+	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 	"path"
-
-	"github.com/RuscalWorld/FabricModManager/config"
-	"github.com/urfave/cli/v2"
 )
 
 var WorkDir string
@@ -33,9 +32,57 @@ func main() {
 					}
 
 					for i, mod := range *mods {
-						fmt.Println(i+1, "|", mod.Name, "|", mod.Description)
+						fmt.Println(i+1, "|", mod.Name, "|", mod.Description, "|", len(mod.Nested), "nested mods")
 					}
 
+					return nil
+				},
+			},
+			{
+				Name:        "check",
+				Aliases:     []string{"c"},
+				Description: "Checks your mod list for conflicts and unmet dependencies",
+				Action: func(context *cli.Context) error {
+					modMap, err := GetFullModMap(nil)
+					if err != nil {
+						return err
+					}
+
+					modMap["minecraft"] = FabricMod{}
+					modMap["fabricloader"] = FabricMod{}
+					errors, warnings := 0, 0
+
+					for _, mod := range modMap {
+						for id := range mod.Breaks {
+							if value, ok := modMap[id]; ok {
+								fmt.Println("!!!", value.Name, "is incompatible with", mod.Name)
+								errors++
+							}
+						}
+
+						for id := range mod.Conflicts {
+							if value, ok := modMap[id]; ok {
+								fmt.Println(value.Name, "is conflicting with", mod.Name)
+								warnings++
+							}
+						}
+
+						for id, version := range mod.Recommends {
+							if _, ok := modMap[id]; !ok {
+								fmt.Println(id, version, "is recommended to be installed with", mod.Name)
+								warnings++
+							}
+						}
+
+						for id, version := range mod.Depends {
+							if _, ok := modMap[id]; !ok {
+								fmt.Println("!!!", id, version, "is required for", mod.Name)
+								errors++
+							}
+						}
+					}
+
+					fmt.Println(errors, "errors and", warnings, "warnings found while checking your mod list")
 					return nil
 				},
 			},
--- mods.go
+++ mods.go
@@ -2,59 +2,53 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"path"
 	"path/filepath"
 )
 
 type FabricMod struct {
-	Environment string      `json:"environment"`
-	Depends     interface{} `json:"depends"`
-	Recommends  interface{} `json:"recommends"`
-	Suggests    interface{} `json:"suggests"`
-	Breaks      interface{} `json:"breaks"`
-	Conflicts   interface{} `json:"conflicts"`
-	Name        string      `json:"name"`
-	Description string      `json:"description"`
+	ID          string                 `json:"id"`
+	Version     string                 `json:"version"`
+	Environment string                 `json:"environment"`
+	Depends     map[string]interface{} `json:"depends"`
+	Recommends  map[string]interface{} `json:"recommends"`
+	Suggests    map[string]interface{} `json:"suggests"`
+	Breaks      map[string]interface{} `json:"breaks"`
+	Conflicts   map[string]interface{} `json:"conflicts"`
+	JARs        *[]NestedJAR           `json:"jars"`
+	Name        string                 `json:"name"`
+	Description string                 `json:"description"`
+	Nested      []FabricMod
 }
 
-func (m FabricMod) GetDepends() []string {
-	return StringOrArray(m.Depends)
-}
-
-func (m FabricMod) GetRecommends() []string {
-	return StringOrArray(m.Recommends)
-}
-
-func (m FabricMod) GetSuggests() []string {
-	return StringOrArray(m.Suggests)
-}
-
-func (m FabricMod) GetBreaks() []string {
-	return StringOrArray(m.Breaks)
-}
-
-func (m FabricMod) GetConflicts() []string {
-	return StringOrArray(m.Conflicts)
-}
-
-func StringOrArray(input interface{}) []string {
-	str, ok := input.(string)
-	if ok {
-		return []string{str}
-	}
-	return input.([]string)
+type NestedJAR struct {
+	File string `json:"file"`
 }
 
 func GetModInfo(path string) (*FabricMod, error) {
-	reader, err := zip.OpenReader(path)
+	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	defer reader.Close()
+	return ReadModInfo(file)
+}
+
+func ReadModInfo(input []byte) (*FabricMod, error) {
+	reader, err := zip.NewReader(bytes.NewReader(input), int64(len(input)))
+	if err != nil {
+		return nil, err
+	}
+
+	var mod *FabricMod
+	nested := make([]FabricMod, 0)
+	nestedPaths := make([]string, 0)
+
 	for _, file := range reader.File {
 		if file.Name == "fabric.mod.json" {
 			file, err := file.Open()
@@ -62,22 +56,65 @@ func GetModInfo(path string) (*FabricMod, error) {
 				return nil, err
 			}
 
-			bytes, err := ioutil.ReadAll(file)
+			data, err := ioutil.ReadAll(file)
 			if err != nil {
 				return nil, err
 			}
 
 			info := &FabricMod{}
-			err = json.Unmarshal(bytes, info)
+			err = json.Unmarshal(data, info)
 			if err != nil {
 				return nil, err
 			}
 
-			return info, nil
+			mod = info
+
+			if mod.JARs != nil {
+				for _, jar := range *mod.JARs {
+					nestedPaths = append(nestedPaths, jar.File)
+				}
+			}
+
+			break
 		}
 	}
 
-	return nil, errors.New("Input JAR file wasn't a Fabric mod ")
+	for _, file := range reader.File {
+		isNestedJar := false
+		for _, nestedPath := range nestedPaths {
+			if nestedPath == file.Name {
+				isNestedJar = true
+			}
+		}
+
+		if !isNestedJar {
+			continue
+		}
+
+		file, err := file.Open()
+		if err != nil {
+			continue
+		}
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			continue
+		}
+
+		nestedMod, err := ReadModInfo(data)
+		if err != nil {
+			continue
+		}
+
+		nested = append(nested, *nestedMod)
+	}
+
+	if mod == nil {
+		return nil, errors.New("Input JAR file wasn't a Fabric mod ")
+	} else {
+		mod.Nested = nested
+		return mod, nil
+	}
 }
 
 func GetMods(dirname string) (*[]FabricMod, error) {
@@ -88,12 +125,12 @@ func GetMods(dirname string) (*[]FabricMod, error) {
 
 	mods := make([]FabricMod, 0)
 	for _, file := range files {
-		path, err := filepath.Abs("mods/" + file.Name())
+		modsPath, err := filepath.Abs("mods/" + file.Name())
 		if err != nil {
 			continue
 		}
 
-		mod, err := GetModInfo(path)
+		mod, err := GetModInfo(modsPath)
 		if err != nil {
 			fmt.Println("Unable to read", file.Name()+":", err)
 			continue
@@ -103,4 +140,31 @@ func GetMods(dirname string) (*[]FabricMod, error) {
 	}
 
 	return &mods, nil
+}
+
+func GetFullModMap(mods *[]FabricMod) (map[string]FabricMod, error) {
+	if mods == nil {
+		var err error
+		mods, err = GetMods(path.Join(WorkDir, "mods"))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	modMap := make(map[string]FabricMod)
+	for _, mod := range *mods {
+		modMap[mod.ID] = mod
+		if len(mod.Nested) > 0 {
+			nestedModMap, err := GetFullModMap(&mod.Nested)
+			if err != nil {
+				continue
+			}
+
+			for id, nestedMod := range nestedModMap {
+				modMap[id] = nestedMod
+			}
+		}
+	}
+
+	return modMap, nil
 }
